c62b3437883bc34d100d334dceeafc617f524dc9
Mateus Oliveira Irineu <20252014040010@ifrn.local>
2025-11-05 14:20:33 -0300

    .

--- ppc/code_lista6/e.py
+++ ppc/code_lista6/e.py
@@ -1,1 +1,10 @@
-# ihghgf
+def dia_da_semana(h, d):
+    semana = ["Domingo", "Segunda-feira", "Terca-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sabado"]
+    hoje = semana.index(h)
+    resto_semana = (hoje + d) % 7
+    resultado = semana[resto_semana]
+    return resultado
+
+print(dia_da_semana("Domingo", 10))
+print(dia_da_semana("Sabado", 21))
+print(dia_da_semana("Quarta-feira", 30))
